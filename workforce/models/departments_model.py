1723699b000444a76a5b3c6b514981fb3a02b89b
CosignMyCodesign <dillonwilliamsail@gmail.com>
2019-01-28 13:13:43 -0600

    added docstrings

--- workforce/models/departments_model.py
+++ workforce/models/departments_model.py
@@ -1,8 +1,25 @@
 from django.db import models
 
 class Department(models.Model):
+    """
+    
+    This creates the Department table
+    
+    Author: Dillon Williams
+    methods: 
+        __str__:
+    
+    """ 
+
     name = models.CharField(max_length=100)
     budget = models.IntegerField()
+
+    """
+    
+    Purpose: converts data to string, 
+    Arguments: self
+    
+    """
 
     def __str__(self):
         return self.name
