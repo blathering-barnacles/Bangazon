b58fd1acab70b80eea303ac8e90793f4bda2f379
jessicabarnett8219 <jessica.barnett8219@gmail.com>
2019-01-29 12:56:18 -0600

    added display of training programs the employee has or is attending

--- workforce/views/employeeDetail_view.py
+++ workforce/views/employeeDetail_view.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, get_object_or_404
-from workforce.models import Employee
+from workforce.models import Employee, EmployeeTrainingProgram
 
 
 def employeeDetail(request, employee_id):
     employee = get_object_or_404(Employee, pk=employee_id)
-    context = { 'employee': employee }
+    training_programs = EmployeeTrainingProgram.objects.filter(employeeId=employee_id)
+    context = { 'employee': employee, 'training_programs': training_programs }
     return render(request, 'workforce/employeeDetail_template.html', context)
