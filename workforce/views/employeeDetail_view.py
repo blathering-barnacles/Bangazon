8a1398694b8c5e148b39af1f5604429776b05d37
jessicabarnett8219 <jessica.barnett8219@gmail.com>
2019-01-29 14:07:26 -0600

    merged with master to get new models

--- workforce/views/employeeDetail_view.py
+++ workforce/views/employeeDetail_view.py
@@ -3,6 +3,16 @@ from workforce.models import Employee, EmployeeTrainingProgram
 
 
 def employeeDetail(request, employee_id):
+    '''[summary]
+    
+    Arguments:
+        request {[type]} -- [description]
+        employee_id {[type]} -- [description]
+    
+    Returns:
+        [type] -- [description]
+    '''
+
     employee = get_object_or_404(Employee, pk=employee_id)
     training_programs = EmployeeTrainingProgram.objects.filter(employeeId=employee_id)
     context = { 'employee': employee, 'training_programs': training_programs }
