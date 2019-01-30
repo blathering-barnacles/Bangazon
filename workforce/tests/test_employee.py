308337c323c12631345161268627b2d979c388c4
rjlancaster <rjlancaster@gmail.com>
2019-01-30 09:49:28 -0600

    Edited test file to clean things up

--- workforce/tests/test_employee.py
+++ workforce/tests/test_employee.py
@@ -3,24 +3,7 @@ from django.test import TestCase
 from django.urls import reverse
 from ..models import Employee, Department
 
-# Stuff to test
-# context: what we send to the template
-# content: the rendered html
-# response_codes
-
-# Name your tests like this! test_foo so Django can find and run 'em
-
-# * The test client is a Python class that acts as a dummy Web browser, allowing you to test your views and interact with your Django - powered application programmatically. Some of the things you can do with the test client are:
-#     * Simulate GET and POST requests on a URL and observe the response – everything from low - level HTTP(result headers and status codes) to page content.
-#     * See the chain of redirects(if any) and check the URL and status code at each step.
-#     * Test that a given request is rendered by a given Django template, with a template context that contains certain values.
-
-# *  Good rules-of-thumb include having:
-#     * a separate TestClass for each model or view
-#     * a separate test method for each set of conditions you want to test
-#     * test method names that describe their function
-
-class EmployeeListTest(TestCase):
+class EmployeeTest(TestCase):
 
     def test_list_employee(self):
         """adds new employee and department to temp database and confirms accurate response
@@ -39,21 +22,10 @@ class EmployeeListTest(TestCase):
             budget=100000
         )
 
-        # Issue a GET request. "client" is a dummy web browser
-        # 'reverse' is used to generate a URL for a given view. The main advantage is that you do not hard code routes in your code.
         response = self.client.get(reverse('workforce:employeeList'))
 
-        # Check that the response is 200 OK.
-        self.assertEqual(response.status_code, 200)
-
-        # Check that the rendered context contains 1 artist.
-        # Response.context is the context variable passed to the template by the view. This is incredibly useful for testing, because it allows us to confirm that our template is getting all the data it needs.
+        self.assertEqual(response.status_code, 200)  # Check that the response is 200 OK.
         self.assertEqual(len(response.context['all_employees']), 1)
-
-        # .encode converts from unicode to utf-8
-        # example:
-        # If the string is: pythön!
-        # The encoded version is: b'pyth\xc3\xb6n!'
         self.assertIn(new_employee.firstName.encode(), response.content)
         self.assertIn(new_employee.lastName.encode(), response.content)
         self.assertIn(new_department.name.encode(), response.content)
