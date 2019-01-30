9b46df0469f4545ebe2c2a3e7f240634fcb6e690
jessicabarnett8219 <jessica.barnett8219@gmail.com>
2019-01-30 15:29:46 -0600

    fixed issue with test_employee file again; merged with new master

--- workforce/tests/test_employee.py
+++ workforce/tests/test_employee.py
@@ -45,34 +45,34 @@ class EmployeeTest(TestCase):
         self.assertIn(new_employee.department.name.encode(), response.content)
         self.assertEqual(response.context["training_programs"][0].trainingProgram.name, new_training.name)
 
-        def test_list_employee(self):
-            """
+    def test_list_employee(self):
+        """
 
-            adds new employee and department to temp database and confirms accurate response
-            code, addition of employee to table and confirms that accurate first name, last name and
-            department are being returned
+        adds new employee and department to temp database and confirms accurate response
+        code, addition of employee to table and confirms that accurate first name, last name and
+        department are being returned
 
-            """
-            new_employee = Employee.objects.create(
-                firstName="Richard",
-                lastName="Lancaster",
-                startDate="1998-08-17",
-                isSupervisor="1",
-                department_id="1"
-            )
+        """
+        new_employee = Employee.objects.create(
+            firstName="Richard",
+            lastName="Lancaster",
+            startDate="1998-08-17",
+            isSupervisor="1",
+            department_id="1"
+        )
 
-            new_department = Department.objects.create(
-                name="Accounting",
-                budget=100000
-            )
+        new_department = Department.objects.create(
+            name="Accounting",
+            budget=100000
+        )
 
-            response = self.client.get(reverse('workforce:employeeList'))
+        response = self.client.get(reverse('workforce:employeeList'))
 
-            self.assertEqual(response.status_code, 200)  # Check that the response is 200 OK.
-            self.assertEqual(len(response.context['all_employees']), 1)
-            self.assertIn(new_employee.firstName.encode(), response.content)
-            self.assertIn(new_employee.lastName.encode(), response.content)
-            self.assertIn(new_department.name.encode(), response.content)
+        self.assertEqual(response.status_code, 200)  # Check that the response is 200 OK.
+        self.assertEqual(len(response.context['all_employees']), 1)
+        self.assertIn(new_employee.firstName.encode(), response.content)
+        self.assertIn(new_employee.lastName.encode(), response.content)
+        self.assertIn(new_department.name.encode(), response.content)
 
     # def test_get_artist_form(self):
 
