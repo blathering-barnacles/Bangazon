f8e199c53d4ebe189533e3adb67db808c141dc1a
buffard <samuelwebber19@gmail.com>
2019-01-30 10:07:37 -0600

    needed to make a few changes with updated master

--- workforce/urls.py
+++ workforce/urls.py
@@ -6,8 +6,8 @@ from . import views
 app_name = 'workforce'
 urlpatterns = [
     path('training/', views.trainingList, name='training'),
-    path('addtraining/', views.newTraining, name='addTraining')
 	path('employees/', views.employeeList, name='employeeList'), #to load the page with employee list
+    path('addtraining/', views.newTraining, name='addTraining')
 ]
 
 
--- workforce/views/__init__.py
+++ workforce/views/__init__.py
@@ -1,5 +1,2 @@
-<<<<<<< HEAD
 from .trainingProgram_views import trainingList, newTraining
-=======
 from .employeeList_view import employeeList
->>>>>>> master
