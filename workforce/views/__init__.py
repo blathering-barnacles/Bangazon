60b8237f4a611c527656a45f853c8762b942db30
Alfonso Miranda <Gabmin21@hotmail.com>
2019-01-31 11:44:53 -0600

    added my edit code to current training program sam's

--- workforce/views/__init__.py
+++ workforce/views/__init__.py
@@ -3,4 +3,4 @@ from .employeeDetail_view import employeeDetail
 from .departmentList_view import departmentList
 from .trainingProgram_views import trainingList, newTraining, pastTrainingList
 from .employeeList_view import employeeList
-from .trainingProgramDetail_view import programsDetail
+from .trainingProgramDetail_view import programsDetail, editProgramForm, editProgram
