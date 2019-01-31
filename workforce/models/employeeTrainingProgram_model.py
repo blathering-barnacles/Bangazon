acd0baf6778aee60094fc0a376019952cdfc98db
Alfonso Miranda <Gabmin21@hotmail.com>
2019-01-31 15:48:05 -0600

    fixed delte issues

--- workforce/models/employeeTrainingProgram_model.py
+++ workforce/models/employeeTrainingProgram_model.py
@@ -5,13 +5,13 @@ from .employee_model import Employee
 from .trainingProgram_model import TrainingProgram
 
 
-class EmployeeTrainingProgram(SafeDeleteModel):
+class EmployeeTrainingProgram(models.Model):
     """
     A model that defines a many-to-many relationship between our Employee and Training Program models and will create a table in our database
     Author: S.W., R.L.
     methods: null
     """
-    _safedelete_policy = HARD_DELETE_NOCASCADE
+    # _safedelete_policy = HARD_DELETE_NOCASCADE
     employee = models.ForeignKey("Employee", on_delete=models.CASCADE)
     trainingProgram = models.ForeignKey("TrainingProgram", on_delete=models.CASCADE)
 
--- workforce/models/trainingProgram_model.py
+++ workforce/models/trainingProgram_model.py
@@ -3,14 +3,14 @@ from safedelete.models import SafeDeleteModel
 from safedelete.models import HARD_DELETE_NOCASCADE
 
 
-class TrainingProgram(SafeDeleteModel):
+class TrainingProgram(models.Model):
     """
     A model that defines a Training Program and will create a table in our database with the same name
     Author: S.W., R.L.
     methods: __str__
     """
 
-    _safedelete_policy = HARD_DELETE_NOCASCADE
+    # _safedelete_policy = HARD_DELETE_NOCASCADE
     name = models.CharField(max_length=35)
     startDate = models.DateField()
     endDate = models.DateField()
