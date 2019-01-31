ad82c2113a402d19a7667d4b3f5db547bde0cbca
Alfonso Miranda <Gabmin21@hotmail.com>
2019-01-31 11:29:09 -0600

    added safe delete to model

--- bangazon_1/settings.py
+++ bangazon_1/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'safedelete',
     'workforce.apps.WorkforceConfig',
     'django.contrib.admin',
     'django.contrib.auth',
--- workforce/models/department_model.py
+++ workforce/models/department_model.py
@@ -1,27 +1,30 @@
 from django.db import models
+from safedelete.models import SafeDeleteModel
+from safedelete.models import HARD_DELETE_NOCASCADE
 
-class Department(models.Model):
+class Department(models.Model, SafeDeleteModel):
     """
 
     Summary:
         This class creates the Department table
 
-    Author: 
+    Author:
         Dillon Williams
-    
+
     methods:
         __str__: computes the “informal” or nicely printable string representation of an object. The return value must be a string object.
 
     """
+    _safedelete_policy = HARD_DELETE_NOCASCADE
     name = models.CharField(max_length=100)
     budget = models.IntegerField()
 
     """
 
-    Purpose: 
+    Purpose:
         converts data to string,
-    Arguments: 
-        self: The first argument of every class method, including init, is always a reference to the current instance of the class. By convention, this argument is always named 'self'. 
+    Arguments:
+        self: The first argument of every class method, including init, is always a reference to the current instance of the class. By convention, this argument is always named 'self'.
 
     """
 
