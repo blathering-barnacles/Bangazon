b8ae37c2172f51d21c3f554f40d4b9730d921b8f
buffard <samuelwebber19@gmail.com>
2019-01-31 10:33:10 -0600

    fixed a broken test

--- workforce/tests/test_trainingProgram.py
+++ workforce/tests/test_trainingProgram.py
@@ -15,24 +15,22 @@ class TrainingTest(TestCase):
         )
         response = self.client.get(reverse('workforce:training'))
 
-       # Check that the response is 200 OK.
+        # Check that the response is 200 OK.
         self.assertEqual(response.status_code, 200)
 
         self.assertEqual(len(response.context['training_list']), 1)
-        
-        test_me = b'<h2>View All Training Programs</h2>\n\n<ul>\n    \n    <li>\n        <a href="/workforce/programs/1/">\n            Coding with dummy code\n        </a>\n    </li>\n    \n</ul>\n\n<a href="/workforce/addtraining">Create New Training Program</a>'
+
+        # if this breaks checks print out your response.content to see if it has changed and adjust test_me to match
+        test_me = b'\n<h2>View Upcoming Training Programs</h2>\n\n<ul>\n    \n    <li>\n        <a href="/workforce/programs/1/">\n            Coding with dummy code\n        </a>\n    </li>\n    \n</ul>\n\n<a href="/workforce/addtraining">Create New Training Program</a>\n</br>\n<a href="/workforce/pastprograms">View Past Training Programs</a>'
         
         self.assertIn(test_me, response.content)
 
     def test_get_program_form(self):
         response = self.client.get(reverse('workforce:addTraining'))
-        # if this breaks check dev tools to get corrected input information
-        print('HERE', response.content)
         
+        # if this breaks checks print out your response.content to see if it has changed and adjust test_form to match
         test_form = '<label>Event Name:\n        <input type="text" name="trainName" />\n    </label>\n    <label>Event Start Date:\n        <input type="date" name="trainStart" />\n    </label>\n    <label>Event End Date:\n        <input type="date" name="trainEnd" />\n    </label>\n    <label> Max Attendees:\n        <input type="int" name="trainMax" />\n    </label>\n    <input type="submit" value="SAVE" />\n</form>'.encode()
         
-        # print('TEST', test_form)
-
         self.assertIn(test_form, response.content)
 
 
