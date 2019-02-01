5fe818d3bffc976bb15b492811a825c706caa555
Alfonso Miranda <Gabmin21@hotmail.com>
2019-02-01 09:22:03 -0600

    deleted safe delete from settings

--- bangazon_1/settings.py
+++ bangazon_1/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'safedelete',
     'workforce.apps.WorkforceConfig',
     'django.contrib.admin',
     'django.contrib.auth',
